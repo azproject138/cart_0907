04f9a4b25fe148b074f902fd6b75bb954048a5f1
badut2kilmkm <juanshiho@students.amikom.ac.id>
2024-06-13 10:06:45 +0700

    keranjang_online_0883

--- app/Http/Controllers/Keranjang.php
+++ app/Http/Controllers/Keranjang.php
@@ -10,6 +10,48 @@ use Illuminate\Http\Request;
 
 class Keranjang extends Controller
 {
+        public function get_prov()
+    {
+    $list = ProvinsiModel::select('id', 'name')
+    ->orderBy('name','ASC')
+    ->get();
+    $html_select = '<option value ="">-- Pilih Salah Satu --</option>';
+    foreach ($list as $key => $value) {
+    $html_select .= '<option value = "'.$value->id.'||'.$value->name.'">'.
+    $value->name.'</option>';
+    }
+    return $html_select;
+    }
+
+        public function get_kota($kode)
+    {
+    $list = KotaModel::select('id', 'name')
+    ->where('province_id', $kode)
+    ->orderBy('name','ASC')
+    ->get();
+    $html_select = '<option value ="">-- Pilih Salah Satu --</option>';
+    foreach ($list as $key => $value) {
+    $html_select .= '<option value = "'.$value->id.'||'.$value->name.'">'.
+    $value->name.'</option>';
+    }
+    return $html_select;
+    }
+
+
+    public function get_kec($kode)
+    {
+    $list = KecamatanModel::select('id', 'name')
+    ->where('province_id', $kode)
+    ->orderBy('name','ASC')
+    ->get();
+    $html_select = '<option value ="">-- Pilih Salah Satu --</option>';
+    foreach ($list as $key => $value) {
+    $html_select .= '<option value = "'.$value->id.'||'.$value->name.'">'.
+    $value->name.'</option>';
+    }
+    return $html_select;
+    }
+
     /**
      * Display a listing of the resource.
      */
